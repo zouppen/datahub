0f8b9a36b6d25aa434e88edc66d42df6793f3acf
Joel Lehtonen <joel.lehtonen@koodilehto.fi>
2016-08-01 16:28:16 +0300

    Use HTTP redirect to forward to correct page of data

--- wuclient.php
+++ wuclient.php
@@ -2,6 +2,8 @@
 header("Content-type: text/csv; charset=utf-8");
 require_once(__DIR__.'/common.php');
 
+$page_size = 20;
+
 // Connect to the queue first (new data may arrive while this script is running)
 $context = new ZMQContext();
 $subscriber = new ZMQSocket($context, ZMQ::SOCKET_SUB);
@@ -9,16 +11,35 @@ $subscriber->connect("ipc://".__DIR__."/updates.ipc");
 $subscriber->setSockOpt(ZMQ::SOCKOPT_SUBSCRIBE, "");
 
 // Prepare statements
-$get_newer_stmt = $db->prepare("SELECT rowid, ts, value from temp WHERE rowid > ? LIMIT 20");
-$get_today_stmt = $db->prepare("SELECT * FROM temp WHERE ts >=strftime('%s','now','start of day')");
+$get_newer_stmt = $db->prepare("SELECT rowid, ts, value from temp WHERE rowid > ? LIMIT $page_size");
+$get_ptr_stmt = $db->prepare("SELECT rowid FROM temp WHERE ts >= ? LIMIT 1;");
+$get_last_stmt = $db->prepare("SELECT MAX(rowid) FROM temp");
 
 // TODO support query by date and add intelligent headers
+// "since" query would return Location: rewrite to mathcnig rowid (rounded down to nearest 1000 for example
 
 // Fetch historical data
 $raw_row = @$_GET['r'];
-$res = $raw_row === NULL ?
-    db_execute($get_today_stmt): // Get today's historic data
-    db_execute($get_newer_stmt, [intval($raw_row)]); // Use provided pointer
+$raw_ts = @$_GET['t'];
+
+if ($raw_ts !== NULL) {
+    // Search by timestamp
+
+    // Atomically get pointer and latest rowid.
+    $db->exec('BEGIN');
+    $ptr = db_execute($get_ptr_stmt, [intval($raw_ts)])->fetchArray(SQLITE3_NUM)[0];
+    $last = db_execute($get_last_stmt)->fetchArray(SQLITE3_NUM)[0];
+    $db->exec('END');
+
+    $final_ptr = $ptr === NULL ? $last : floor($ptr / $page_size) * $page_size - 1;
+    header("Location: ?r=$final_ptr");
+    exit(0);
+} else if ($raw_row === NULL) {
+    print("Provide either r or t parameter\n");
+    exit(0);
+}
+
+$res = db_execute($get_newer_stmt, [intval($raw_row)]); // Use provided pointer
 $got = FALSE;
 while (true) {
     $row = $res->fetchArray(SQLITE3_NUM);
